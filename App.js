5aaaed6bf5b6bef29bc2fbfabce650f6b6952e26
Asad Iqbal <asad.dsoft@gmail.com>
2020-09-02 13:59:32 -0500

    updated readme

--- App.js
+++ App.js
@@ -33,114 +33,55 @@ const App: () => React$Node = () => {
   const [soundsList, setSoundsList] = useState({});
 
   useEffect(() => {
-    
-    if (soundsList.length > 0) {
-      //console.warn(soundsList.length)
-      return 
-    }
     NotificationSounds.getNotifications('ringtone').then(sounds => {
       console.warn('SOUNDS', JSON.stringify(sounds));
-      /* 
-      Play the notification sound.
-      pass the complete sound object.
-        This function can be used for playing the sample sound
-    */
-     // playSampleSound(sounds[1]);
       setSoundsList(sounds)
-
     });
+  }, [])
 
-  }, [soundsList])
 
-
-  renderSound = (sound) => {
-    
-    const item = sound.item;
+  renderSound = ({ item }) => {
     // console.warn(item)
-    return (<TouchableOpacity
-      onPress={() => {
-        console.warn(item)
-        playSampleSound(item);
-      }}
-    >
-      <Text style={{ color: 'black'}}>{item.title}</Text>
-    </TouchableOpacity>)
+    return (
+      <TouchableOpacity onPress={() => playSampleSound(item)}>
+          <Text style={styles.soundText}>{item.title}</Text>
+      </TouchableOpacity>
+    )
   }
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            
-            <Button title='Stop' onPress={()=>{ stopSampleSound() }} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Button color={'red'} title='Stop Sound' onPress={()=>{ stopSampleSound() }} />
             <FlatList
               data={soundsList}
               renderItem={renderSound}
-              style={{ flex: 1 }}
-              contentContainerStyle={{ flexGrow: 1 }}
+              style={styles.soundsList}
+              contentContainerStyle={{ flexGrow: 1, paddingVertical: 8 }}
               keyExtractor={item => item.soundID}
               />
-
-          </View>
-        </ScrollView>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
+  
+  soundsList: {
     backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    paddingHorizontal: 24
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
+  
+  soundText: {
     marginTop: 8,
     fontSize: 18,
+    paddingVertical: 4,
     fontWeight: '400',
-    color: Colors.dark,
+    color: '#ae00ff',
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  
+  
 });
 
 export default App;
---
+++ README.md
@@ -0,0 +1,20 @@
+
+React native  app that demonstrate the usage of `react-native-notification-sounds`
+
+## Installation
+
+`$ git clone https://github.com/saadqbal/notificationSounds.git`
+`$ cd notificationSounds`
+`$ npm install`
+`$ cd ios`
+`$ pod install`
+### For Android
+`$ react-native run-android`
+### For iOS
+`$ react-native run-ios`
+
+
+## 📄 License
+
+ 
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/saadqbal/notificationSounds/blob/master/LICENSE) file for details
